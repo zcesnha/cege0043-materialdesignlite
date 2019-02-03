a1683e8a5dcb0629f98a9128fe2eb783b61d6476
zcesnha <nicholas.hay.17@ucl.ac.uk>
2019-02-03 17:04:41 -0500

    Creating Div Change Function

---
+++ zcesnha/www/js/rhMenu.js
@@ -0,0 +1,9 @@
+//creating Pop-up alert 
+function menuClicked() {
+alert("You clicked the menu");
+} 
+
+//creating UCL logo Div Change 
+function addUCLLogo() {
+document.getElementById(“ucllogo”).innerHTML =”<img src=’ images/ucl.png’>”
+}
