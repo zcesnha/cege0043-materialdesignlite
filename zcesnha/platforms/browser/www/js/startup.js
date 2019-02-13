61ccec96fc36c05beebd368b315e86d26c2ee764
zcesnha <nicholas.hay.17@ucl.ac.uk>
2019-02-13 16:42:59 -0500

    Working One Click

---
+++ zcesnha/platforms/browser/www/js/startup.js
@@ -0,0 +1,10 @@
+// creating new function so multiple functions can be performed on one click 
+function trackAndCircle() { 
+
+	//calling track location 
+	trackLocation(); 
+
+	//calling create point, line and poly 
+	addPointLinePoly(); 
+
+}
