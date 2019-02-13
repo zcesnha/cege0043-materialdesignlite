08b9ea2b38411ee0b9740720ddabf5bfcc299a5a
zcesnha <nicholas.hay.17@ucl.ac.uk>
2019-02-13 14:29:13 -0500

    Working Singular Marker

--- zcesnha/www/js/userTracking.js
+++ zcesnha/www/js/userTracking.js
@@ -1,14 +1,21 @@
 function trackLocation() {
-if (navigator.geolocation) {
-navigator.geolocation.watchPosition(showPosition);
-} else {
-document.getElementById('showLocation').innerHTML = "Geolocation is not supported by this browser.";
-}
-}
+	if (navigator.geolocation) {
+	navigator.geolocation.watchPosition(showPosition);
+	} else {
+	document.getElementById('showLocation').innerHTML = "Geolocation is not supported by this browser.";
+	}
+} 
+
+//define global variable 
+var userMarker; 
 
 function showPosition(position) {
-L.marker([position.coords.latitude, position.coords.longitude]).addTo(mymap)
-.bindPopup("<b>You were here</b>");
+	// removes any existing markers (avoids drawing a new marker everytime location changes) 
+	if (userMarker){
+	mymap.removeLayer(userMarker);
+	} 
+
+	userMarker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(mymap).bindPopup("<b>You were here</b>");
 }
 
 
