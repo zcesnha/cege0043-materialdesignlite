7a8d89636196756e3d8e85e78b24028426d140e9
zcesnha <nicholas.hay.17@ucl.ac.uk>
2019-03-09 13:28:46 -0500

    Working DIV for choosing files

--- zcesnha/www/js/testAJAX.js
+++ zcesnha/www/js/testAJAX.js
@@ -8,11 +8,11 @@ xhr.send();
 }
 function processDivNodeJSChange() {
 if (xhr.readyState < 4) // while waiting response from server
- document.getElementById('div1').innerHTML = "Loading...";
+ document.getElementById('ajaxrequest').innerHTML = "Loading...";
  else if (xhr.readyState === 4) { // 4 = Response from server has been completely loaded.
  if (xhr.status == 200 && xhr.status < 300)
 // http status between 200 to 299 are all successful
- document.getElementById('div1').innerHTML = xhr.responseText;
+ document.getElementById('ajaxrequest').innerHTML = xhr.responseText;
  }
 } 
 
